1203c3e63864816757039b87997f1bc7fb92deb3
YTG2G3 <scratchlover05@gmail.com>
2022-12-12 19:02:49 -0800

    stack to drawer nav

---
+++ App.jsx
@@ -0,0 +1,80 @@
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import axios from 'axios';
+import { deleteItemAsync, getItemAsync, setItemAsync } from 'expo-secure-store';
+import { useEffect, useState } from 'react';
+import { Splash } from './screens';
+import { API_URL, GOOGLE_ID } from '@env';
+import { useAuthRequest } from 'expo-auth-session/providers/google';
+import { maybeCompleteAuthSession } from 'expo-web-browser';
+
+maybeCompleteAuthSession();
+
+// Stack navigator
+const Drawer = createDrawerNavigator();
+
+// Background clearing
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        background: 'white'
+    }
+}
+
+export default function App() {
+    let [user, setUser] = useState(undefined);
+
+    // Google Auth
+    const [request, response, promptAsync] = useAuthRequest({
+        expoClientId: GOOGLE_ID,
+        iosClientId: GOOGLE_ID,
+        androidClientId: GOOGLE_ID,
+        responseType: "id_token token",
+        usePKCE: false,
+        extraParams: { nonce: Math.floor(Math.random() * 16).toString(16) }
+    });
+
+    // Check previous login / start new login
+    const loadUser = async () => {
+        let saved = await getItemAsync("user"); // token saved in secure store?
+
+        if (saved) getUserFromToken();
+        else promptAsync();
+    }
+
+    // Refresh token with Google OAuth
+    useEffect(() => { if (response) getAzureToken(response) }, [response]);
+
+    const getAzureToken = async (google) => {
+        let { accessToken: access_token, idToken: id_token } = google.authentication;
+        let { data } = await axios.post(`${API_URL}/.auth/login/google`, { access_token, id_token });
+
+        getUserFromToken(data.authenticationToken);
+    }
+
+    // Token verification and retrieve user info if valid
+    const getUserFromToken = async (token) => {
+        try {
+            let { data } = await axios.get(`${API_URL}/supervisor`, { headers: { "X-ZUMO-AUTH": token } });
+            await setItemAsync("user", token); // save for persistency
+            setUser(data);
+        } catch (error) {
+            if (error.response.status === 401) {
+                deleteItemAsync("user");
+                promptAsync();
+            }
+            else alert("Unknown error occured"); // TODO - prettify
+        }
+    }
+
+    if (!user) return <Splash cb={loadUser} />
+
+    return (
+        <NavigationContainer theme={theme}>
+            <Drawer.Navigator screenOptions={{ headerShown: false }}>
+                <Drawer.Screen name="Splash" component={Splash} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
+}
---
+++ screens/splash.jsx
@@ -0,0 +1,45 @@
+import { StatusBar } from "expo-status-bar";
+import { Image, SafeAreaView, View } from "react-native";
+import styles from '../styles/splash.scss';
+import { Dimensions, ActivityIndicator } from "react-native";
+import { useEffect, useState } from "react";
+import { useSpring, animated, config } from "@react-spring/native";
+
+const { width, height } = Dimensions.get('window');
+const logoDim = Math.min(width, height);
+const logoMax = 250;
+
+export default function Splash({ cb }) {
+    let [loading, setLoading] = useState(false);
+
+    let fadeAnim = useSpring({
+        from: { opacity: 0 },
+        to: { opacity: 1 },
+        delay: 100,
+        config: config.molasses
+    });
+
+    let jumpAnim = useSpring({
+        from: { marginTop: 100 },
+        to: { marginTop: 0 },
+        delay: 100,
+        config: { friction: 10 },
+        onRest: () => {
+            setTimeout(() => {
+                cb();
+                setLoading(true);
+            }, 1000);
+        }
+    })
+
+    return (
+        <SafeAreaView>
+            <StatusBar hidden />
+
+            <animated.View style={{ ...styles.container, ...fadeAnim, ...jumpAnim }}>
+                <Image style={{ width: logoDim * 0.8, height: logoDim * 0.8, maxWidth: logoMax, maxHeight: logoMax }} source={require('../assets/splash.jpg')} />
+                <ActivityIndicator animating={loading} />
+            </animated.View>
+        </SafeAreaView>
+    );
+}
